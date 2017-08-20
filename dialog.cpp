078f09c868d7b10b5f4fb99bea18490674c3a058
sven zhang <qqwowo2011@outlook.com>
2017-08-20 23:21:51 +0800

    add tray functions

--- dialog.cpp
+++ dialog.cpp
@@ -1,12 +1,25 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include <QMessageBox>
+#include <QCloseEvent>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    ui->progressBar->setValue(0);
+//    setAttribute(Qt::WA_TranslucentBackground);
+//    setWindowFlags(Qt::FramelessWindowHint);
+
+    isLogin = true;
+
+    iconCorrect = QIcon(":/resource/correct.png");
+    iconError = QIcon(":/resource/error.png");
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setToolTip("单击显示系统信息\n右击显示功能菜单");
+    setBothIcons(0);
+    trayIcon->setVisible(true);
 }
 
 Dialog::~Dialog()
@@ -29,4 +42,52 @@ void Dialog::on_pushButton_clicked()
 void Dialog::on_pushButton_2_clicked()
 {
 
+}
+
+void Dialog::logout()
+{
+    isLogin = false;
+    showNormal();
+}
+
+void Dialog::setBothIcons(int index)
+{
+    (index==0)?setWindowIcon(iconCorrect):setWindowIcon(iconError);
+    (index==0)?trayIcon->setIcon(iconCorrect):trayIcon->setIcon(iconError);
+}
+
+void Dialog::setupTrayMenu()
+{
+    userMsg = new QAction("当前用户：", this);
+    deviceMsg = new QAction("当前设备：", this);
+    logoutAction = new QAction("注销用户", this);
+    quitAction = new QAction("退出程序", this);
+
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addAction(userMsg);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(deviceMsg);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(logoutAction);
+    trayIconMenu->addAction(quitAction);
+
+    connect(logoutAction, SIGNAL(triggered()),this,SLOT(logout()));
+    connect(quitAction, SIGNAL(triggered()),qApp, SLOT(quit()));
+
+    trayIcon->setContextMenu(trayIconMenu); // 将tray图标设为菜单内容
+}
+
+void Dialog::closeEvent(QCloseEvent *event)
+{
+    if(trayIcon->isVisible())
+    {
+        if(isLogin)
+            hide();
+        else
+        {
+            QMessageBox::information(this,tr("远程传输与控制系统"),
+                                     tr("请登录。\n\n退出程序:请右击系统托盘图标，选择退出程序。"));
+        }
+//        event->ignore();
+    }
 }
--- dialog.h
+++ dialog.h
@@ -2,6 +2,10 @@
 #define Dialog_H
 
 #include <QDialog>
+#include <QSystemTrayIcon>
+#include <QAction>
+#include <QMenu>
+
 
 namespace Ui {
 class Dialog;
@@ -19,10 +23,24 @@ private slots:
     void on_pushButton_clicked();
 
     void on_pushButton_2_clicked();
-
+    void logout();
+    void closeEvent(QCloseEvent *event);
 private:
     bool isLogin;
     Ui::Dialog *ui;
-};
 
+    QAction *userMsg;
+    QAction *deviceMsg;
+    QAction *logoutAction;
+    QAction *quitAction;
+
+    QMenu *trayIconMenu;
+
+    QIcon iconCorrect;
+    QIcon iconError;
+    QSystemTrayIcon *trayIcon;
+protected:
+    void setBothIcons(int index);       //设置程序图标（窗口图标 + 托盘图标）
+    void setupTrayMenu();
+};
 #endif // Dialog_H
